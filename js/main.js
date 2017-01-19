fd7f7063fda5c0bfa28e1a4e7be25347d775d81c
Oare Rodney Arene <rodneyoare@gmail.com>
2017-01-19 21:54:00 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 $(function(){
 	$("#quote").typed({
-		strings: ["Hi! Oare is my name,<br/>design is my game.", "I am awesome.", "This site is under construction."],
+		strings: ["My name is Oahray,<br/>I like creating stuff.", "I'd have loved to invite you over for dinner but I'm a little...  BUSY.", "This site is under construction, please hang on.", "Ok... Bye],
 		typeSpeed: 40,
 		loop : true,
 	});
