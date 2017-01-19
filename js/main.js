a0cac586608a58e109d2b4306d71cf9ef66d633c
Oare Rodney Arene <rodneyoare@gmail.com>
2017-01-19 21:15:53 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
+
+$(function(){
+	$("#quote").typed({
+		strings: ["Hi! Oare is my name,<br/>design is my game.", "I am awesome.", "This site is under construction."],
+		typeSpeed: 40,
+		loop : true,
+	});
+});
+
 $( document ).ready(
 	$(".dropdown-button").dropdown(){
-		hover: false
-	};
-
-	$(function(){
-		$("#quote").typed({
-			strings: ["Hi! Oare is my name,<br/>design is my game.", "I am awesome.", "This site is under construction."],
-			typeSpeed: 40,
-			loop : true,
-		});
+		hover: false;
 	});
-);
