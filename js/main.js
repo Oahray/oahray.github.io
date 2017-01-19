95016124e5e148eccb9e33e229775acd5d57a9dc
Oahray <rodneyoare@gmail.com>
2017-01-19 20:45:08 +0100

    modified index.html

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+	$("#quote").typed({
+		strings: ["Hi! Oare is my name,<br/>design is my game.", "I am awesome.", "This site is under construction."],
+		typeSpeed: 40,
+		loop : true,
+	});
+});
